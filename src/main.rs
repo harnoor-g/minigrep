83ebb756c117452f0f24d086401b93a29e97e463
Harnoor <gill_h@pm.me>
2023-05-20 11:24:57 +1200

    refactoring to improve modularity and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("searching for '{}' in file '{}'", query, file_path);
+    println!("searching for '{}' in file '{}'", config.query, config.file_path);
 
-    let contents = fs::read_to_string(file_path)
-        .expect("should have been able to read the file");
-    
-    println!("with text:\n{contents}");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
